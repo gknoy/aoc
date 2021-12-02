cbe2e9a12bbc1c4b2f2c66383e94a4f18eb87bbd
Gabriel Knoy <gabriel.knoy@gmail.com>
2021-12-02 10:17:20 -0800

    Day 1, part 2. Refactored part 1 and 2 together.

--- advent.py
+++ advent.py
@@ -23,36 +23,56 @@ def get_line_items(fname):
 
 def one():
     """
-    Count how many times measurement is larger than previous measurement
     https://adventofcode.com/2021/day/1
     """
     measurements = [int(item) for item in get_line_items("input/1.txt")]
+    # toy input:
+    # measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-    def count_increasing_measurements(measurements):
+    def count_increasing_items(items):
         return sum(
             1
-            for index, item in enumerate(measurements)
-            if (index > 0 and measurements[index] > measurements[index - 1])
+            for index, item in enumerate(items)
+            if (index > 0 and items[index] > items[index - 1])
         )
 
-    return count_increasing_measurements(measurements)
+    def part_1():
+        """
+        Count how many times measurement is larger than previous measurement
+        """
+        return count_increasing_items(measurements)
 
+    def part_2():
+        """
+        Count increasing sums of N-measurement windows in the input
+        """
 
-def one_b():
-    """
-    Count increasing sums of N-measurement windows in the input
-    """
-    measurements = [int(item) for item in get_line_items("input/1.txt")]
+        def window_slicer(size):
+            # return a sliding window slicer
+            def slicer(index, items):
+                return items[index : index + size]
+
+            return slicer
+
+        window_size = 3
+        slicer = window_slicer(window_size)
+
+        windows = [
+            slicer(index, measurements)
+            for index in range(len(measurements) - window_size + 1)
+        ]
+
+        sliding_window_sums = [sum(window) for window in windows]
+        return count_increasing_items(sliding_window_sums)
+
+    return [part_1(), part_2()]
 
 
 # -----------------------
 # actually run things ...
 # -----------------------
 
-ADVENTS = {
-    "1": one,
-    "1b": one_b,
-}
+ADVENTS = {"1": one}
 
 
 if __name__ == "__main__":
