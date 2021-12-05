6c0b16fd766122d45d9dfedb97515213dd332921
Gabriel Knoy <gabriel.knoy@gmail.com>
2021-12-05 11:52:05 -0800

    Add testing of advents' expected values

---
+++ tests/test_advents.py
@@ -0,0 +1,31 @@
+#
+# Sanity checks that all advents work even after refactoring
+#
+from advent import ADVENTS
+
+TOY_EXPECTED = {
+    1: [7, 5],
+    2: [150, 900],
+    3: [4160394, 4125600],
+    4: [4512, 1924],
+    5: [None, None],
+}
+
+EXPECTED = {
+    1: [1298, 1248],
+    2: [2147104, 2044620088],
+    3: [4160394, 4125600],
+    4: [39984, 8468],
+    5: [None, None],
+}
+
+
+def _advent(item, use_toy_data=False, verbose=False):
+    return ADVENTS[item](use_toy_data=use_toy_data, verbose=verbose)
+
+
+def test_advents():
+    for key in ADVENTS:
+        e_key = int(key)
+        assert TOY_EXPECTED[e_key] == _advent(key, use_toy_data=True)
+        assert EXPECTED[e_key] == _advent(key, use_toy_data=False)
