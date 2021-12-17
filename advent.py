dddf347c9ffaf31b4cbed5dcfc851fc7b6db1e27
Gabriel Knoy <gabriel.knoy@gmail.com>
2021-12-17 14:10:31 -0800

    Refactor advent.py

--- advent.py
+++ advent.py
@@ -15,6 +15,8 @@ from days.day06 import day_6
 from days.day07 import day_7
 from days.day08 import day_8
 
+# Harvest from globals() so that I don't need to add both an import
+# AND a key/value pair each day
 ADVENTS = {
     k.replace("day_", ""): v for k, v in globals().items() if k.startswith("day_")
 }
@@ -30,16 +32,17 @@ def _advent(item, use_toy_data, verbose):
 if __name__ == "__main__":
     use_toy_data = False
     verbose = False
+
+    options = [item for item in sys.argv[1:] if item.startswith("--")]
+    use_all = "--all" in options
+    advents_to_run = [
+        item for item in ADVENTS.keys() if use_all or item in sys.argv
+    ] or ADVENTS.keys()
+
     if "--toy" in sys.argv:
         use_toy_data = True
     if "--verbose" in sys.argv:
         verbose = True
 
-    if "--all" in sys.argv:
-        for item in ADVENTS.keys():
-            _advent(item, use_toy_data, verbose)
-    else:
-        for item in sys.argv[1:]:
-            if item in ["--toy", "--verbose"]:
-                continue
-            _advent(item, use_toy_data, verbose)
+    for item in advents_to_run:
+        _advent(item, use_toy_data, verbose)
