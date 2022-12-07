2b0cc84151b5024b52883b4bfecdc51b33aa6f8c
Gabriel Knoy <gabriel.knoy@gmail.com>
2022-12-07 11:08:38 -0800

    Day 4, part 1

---
+++ aoc_2022/days/day04.py
@@ -0,0 +1,60 @@
+"""
+# https://adventofcode.com/2022/day/4
+"""
+from typing import List
+from utils.utils import get_line_items
+
+input = list(get_line_items("aoc_2022/input/04.txt"))
+toy_input: List[str] = [
+    # fmt: off
+    "2-4,6-8",
+    "2-3,4-5",
+    "5-7,7-9",
+    "2-8,3-7",
+    "6-6,4-6",
+    "2-6,4-8",
+    # fmt: on
+]
+
+
+class Range:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def contains(self, other):
+        return self.start <= other.start and self.end >= other.end
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+
+def parse_pair(line: str) -> List[Range]:
+    tuples = line.split(",")
+    return [Range(*map(int, item.split("-"))) for item in tuples]
+
+
+def test_contains():
+    assert Range(2, 9).contains(Range(3, 7))
+    assert Range(2, 4).contains(Range(2, 4))
+
+
+def test_parse_pair():
+    assert parse_pair("2-4,6-8") == [Range(2, 4), Range(6, 8)]
+
+
+def part_1(input, verbose=False):
+    """
+    Number pairs represent ranges. In how many pairs is one range included in the other one?
+    """
+    pairs = map(parse_pair, input)
+    return sum([1 if a.contains(b) or b.contains(a) else 0 for a, b in pairs])
+
+
+def part_2(input, verbose=False):
+    pass
+
+
+def day_4(use_toy_data=False, verbose=False):
+    data = toy_input if use_toy_data else input
+    return [part_1(data, verbose), part_2(data, verbose)]
