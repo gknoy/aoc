e4285eba97a65ce74cabc63bf9e1d271d57327ca
Gabriel Knoy <gabriel.knoy@gmail.com>
2023-12-01 14:14:40 -0800

    Day 2 solution

--- aoc_2023/days/day01.py
+++ aoc_2023/days/day01.py
@@ -22,32 +22,87 @@ toy_input: list[str] = [
 # the first digit and the last digit (in that order)
 # to form a single two-digit number.
 
-# DIGITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-FIRST_DIGIT = re.compile(r'\d')
-LAST_DIGIT = re.compile(r'.*(\d).*?')
+FIRST_DIGIT_v1 = re.compile(r"\d")
+LAST_DIGIT_v1 = re.compile(r".*(\d).*?")
 
 
 def parse_calibration_v1(line: str) -> int:
     # get first and last digits
-    first = FIRST_DIGIT.search(line).group(0)
+    first = FIRST_DIGIT_v1.search(line).group(0)
     # only one group
     # first group is whole string, second is what we want:
-    last = LAST_DIGIT.search(line).group(1)
-    return int(f"{first}{last}") # concat digits
+    last = LAST_DIGIT_v1.search(line).group(1)
+    return int(f"{first}{last}")  # concat digits
 
 
 def part_1(input, verbose=False):
-    calibration_values = (
-        parse_calibration_v1(line)
-        for line in input
-    )
+    calibration_values = (parse_calibration_v1(line) for line in input)
     return sum(calibration_values)
 
 
+# ==============================
+# Part 2
+# ==============================
+#
+# strings naming digits also count as digits
+
+toy_input_2 = [
+    "two1nine",  # 29
+    "eightwothree",  # 83
+    "abcone2threexyz",  # 13
+    "xtwone3four",  # 24
+    "4nineeightseven2",  # 42
+    "zoneight234",  # 84
+    "7pqrstsixteen",  # 76
+]
+
+FIRST_DIGIT_v2 = re.compile(r"(\d|one|two|three|four|five|six|seven|eight|nine|zero)")
+LAST_DIGIT_v2 = re.compile(
+    r".*(\d|one|two|three|four|five|six|seven|eight|nine|zero).*?"
+)
+DIGITS = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "0": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
+
+
+def parse_calibration_v2(line: str) -> int:
+    """
+    Concatenate first and last found digits, return int(digits)
+    digit names ("one") count as a digit too
+    """
+    # There's a fancy find-all-matching-substrings algorithm
+    # that someone mentioned on HN, but I didn't know about it before
+    # doing this.  Might refactor later if performance bothers me ;)
+    first = DIGITS[FIRST_DIGIT_v2.search(line).group(1)]
+    last = DIGITS[LAST_DIGIT_v2.search(line).group(1)]
+    return int(f"{first}{last}")  # concat digits
+
+
 def part_2(input, verbose=False):
-    pass
+    calibration_values = (parse_calibration_v2(line) for line in input)
+    return sum(calibration_values)
 
 
 def day_1(use_toy_data=False, verbose=False):
-    data = toy_input if use_toy_data else input
-    return [part_1(data, verbose), part_2(data, verbose)]
+    data1 = toy_input if use_toy_data else input
+    data2 = toy_input_2 if use_toy_data else input
+    return [part_1(data1, verbose), part_2(data2, verbose)]
--- aoc_2023/tests/test_day01.py
+++ aoc_2023/tests/test_day01.py
@@ -1,7 +1,7 @@
 """
 # https://adventofcode.com/2023/day/1
 """
-from aoc_2023.days.day01 import input, toy_input, part_1, part_2
+from aoc_2023.days.day01 import input, toy_input, toy_input_2, part_1, part_2
 
 
 def test_part_1_toy():
@@ -9,12 +9,12 @@ def test_part_1_toy():
 
 
 def test_part_1_real():
-    assert part_1(input) == "FIXME"
+    assert part_1(input) == 56465
 
 
 def test_part_2_toy():
-    assert part_2(toy_input) == "FIXME"
+    assert part_2(toy_input_2) == 281
 
 
 def test_part_2_real():
-    assert part_1(input) == "FIXME"
+    assert part_2(input) == 55902
