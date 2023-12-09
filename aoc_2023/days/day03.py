877cbfdb2c86458ff41104efde7d47fcdb374468
Gabriel Knoy <gabriel.knoy@gmail.com>
2023-12-08 17:33:31 -0800

    add longer test of finding part numbers

--- aoc_2023/days/day03.py
+++ aoc_2023/days/day03.py
@@ -2,6 +2,7 @@
 # https://adventofcode.com/2023/day/3
 """
 import functools
+import itertools
 from dataclasses import dataclass
 
 from utils.utils import get_line_items
@@ -34,15 +35,38 @@ class Item:
     start: int = 0
     end: int = 0
 
+    def __hash__(self):
+        return hash((self.start, self.end))
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+    def __lt__(self, other):
+        return self.start < other.start and self.end < other.end
+
+    def __gt__(self, other):
+        return self.start > other.start and self.end > other.end
+
 
 @dataclass
 class Symbol(Item):
     name: str = None
 
+    def __hash__(self):
+        return hash(("S", self.start, self.end, self.name))
+
 
 @dataclass
 class PartNumber(Item):
     value: int | str = None
+
+    def __hash__(self):
+        return hash(("PN", self.start, self.end, self.value))
+
+
+GridRow = list[Symbol | PartNumber]
+Grid = list[GridRow]
+Position = tuple[int, int]  # row, col
 
 
 @functools.cache
@@ -55,36 +79,97 @@ def is_symbol(c: str) -> bool:
     return c != "." and not is_digit(c)
 
 
-def parse_line(line: str) -> list[Item]:
-    digits = ""
-    first = 0
-    last = 0
+def parse_line(line: str) -> GridRow:
+    # the current-parsing-thing (We can just directly store symbols)
+    current: PartNumber | None = None
     found = []
-
-    def _save_part_number():
-        if len(digits):
-            value = int(digits)
-            found.append(PartNumber(start=first, end=last, value=value))
 
     for index in range(len(line)):
         c = line[index]
 
         if is_digit(c):
-            digits += c
-            last = index
-            # WTF... how did this even work right the first time?
+            if current is None:
+                # first digit of new number
+                current = PartNumber(
+                    start=index,
+                    end=index + 1,  # changed once we find end of string
+                    value=int(c),
+                )
+                continue
+            if type(current) is PartNumber:
+                #
+                current.value = int(f"{current.value}{c}")
+                current.end = index + 1
+                continue
         if c == ".":
-            _save_part_number()
-            digits = ""
-            first = last = index
+            if current is None:
+                continue
+            if type(current) is PartNumber:
+                # store it because we're done
+                found.append(current)
+                current = None
         if is_symbol(c):
-            found.append(Symbol(name=c, start=first, end=last))
-            first = last = index
+            # save current if needed
+            if type(current) is PartNumber:
+                # store it because we've found something else
+                found.append(current)
+            # save this symbol too
+            found.append(Symbol(name=c, start=index, end=index + 1))
+            current = None
+    return found
+
+
+def get_adjacent_positions(pos: Position) -> list[Position]:
+    r, c = pos
+    offsets = [-1, 0, 1]
+
+    return [
+        (r + row_offset, c + col_offset)
+        for row_offset in offsets
+        for col_offset in offsets
+        # exclude original position ;)
+        if (r + row_offset, c + col_offset) != (r, c)
+    ]
+
+
+def get_at_position(grid: Grid, pos: tuple[int, int]) -> Item | None:
+    row, col = pos
+    if row < 0 or row >= len(grid):
+        return None
+    # we don't care about row bounds because items store their locations
+    for item in grid[row]:
+        if item.start <= col < item.end:
+            return item
+    return None
+
+
+def get_adjacent_parts(grid: Grid, pos) -> list[PartNumber]:
+    adj_positions = get_adjacent_positions(pos)
+    candidates = set([get_at_position(grid, adj_pos) for adj_pos in adj_positions])
+    return [item for item in candidates if item is not None and type(item) is PartNumber]
+
+
+def find_part_numbers(grid: Grid) -> list[PartNumber]:
+    """
+    Get all part numbers that are adjacent to a symbol
+    Assume some parts can be adjacent to multiple symbols
+    """
+    for row_index in range(len(grid)):
+        for item in grid[row_index]:
+            if type(item) is Symbol:
+                for part in get_adjacent_parts(grid, (row_index, item.start)):
+                    yield part
+
+
+def parse_grid(lines: list[str]) -> Grid:
+    return [parse_line(line) for line in lines]
 
 
 def part_1(input, verbose=False):
-    grid = [parse_line(line) for line in input]
-    return "FIXME"
+    grid = parse_grid(input)
+    part_numbers = list(find_part_numbers(grid))
+    numbers = [part.value for part in part_numbers]
+    return sum(numbers)
 
 
 def part_2(input, verbose=False):
--- aoc_2023/tests/test_day03.py
+++ aoc_2023/tests/test_day03.py
@@ -1,7 +1,20 @@
 """
 # https://adventofcode.com/2023/day/3
 """
-from aoc_2023.days.day03 import input, toy_input, part_1, parse_line, PartNumber, Symbol
+from aoc_2023.days.day03 import (
+    input,
+    toy_input,
+    part_1,
+    part_2,
+    parse_line,
+    parse_grid,
+    PartNumber,
+    Symbol,
+    get_adjacent_positions,
+    get_at_position,
+    get_adjacent_parts,
+    find_part_numbers,
+)
 
 
 def test_parse_line():
@@ -12,14 +25,147 @@ def test_parse_line():
         Symbol(name="+", start=5, end=6),
         PartNumber(value=58, start=7, end=9),
     ]
+    assert parse_line(line) == expected
 
 
-# def test_part_1_toy():
-#     assert part_1(toy_input) == 4361
+def test_get_adjacent_positions():
+    assert get_adjacent_positions((3, 4)) == [
+        # fmt: off
+        (2, 3), (2, 4), (2, 5),
+        (3, 3),         (3, 5),
+        (4, 3), (4, 4), (4, 5),
+        # fmt: on
+    ]
 
 
-# def test_part_1_real():
-#     assert part_1(input) == "FIXME"
+def test_get_at_position():
+    grid = [
+        [
+            PartNumber(value=13, start=1, end=3),
+            Symbol(name="+", start=5, end=6),
+            PartNumber(value=58, start=7, end=9),
+        ],
+    ]
+    assert get_at_position(grid, (0, 2)) == PartNumber(value=13, start=1, end=3)
+    assert get_at_position(grid, (0, 5)) == Symbol(name="+", start=5, end=6)
+    assert get_at_position(grid, (0, 8)) == PartNumber(value=58, start=7, end=9)
+
+
+def test_get_adjacent_parts():
+    #   "467..114..",
+    #   "...*......",  # <<< 35, 467 are both adjacent to star
+    #   "..35..633.",
+    grid = [
+        [
+            PartNumber(value=467, start=0, end=3),
+            PartNumber(value=114, start=5, end=8),
+        ],
+        [Symbol(start=3, end=4, name="*")],
+        [
+            PartNumber(start=2, end=4, value=35),
+            PartNumber(start=6, end=9, value=633),
+        ],
+    ]
+    adj_parts = get_adjacent_parts(grid, (1, 3))
+    assert len(adj_parts) == 2
+    assert set(adj_parts) == set(
+        [
+            PartNumber(value=467, start=0, end=3),
+            PartNumber(start=2, end=4, value=35),
+        ]
+    )
+
+
+def test_find_part_numbers():
+    #   "467..114..",
+    #   "...*......",  # <<< 35, 467 are both adjacent to star
+    #   "..35..633.",
+    grid = [
+        [
+            PartNumber(value=467, start=0, end=3),
+            PartNumber(value=114, start=5, end=8),
+        ],
+        [Symbol(start=3, end=4, name="*")],
+        [
+            PartNumber(start=2, end=4, value=35),
+            PartNumber(start=6, end=9, value=633),
+        ],
+    ]
+    parts = list(find_part_numbers(grid))
+    assert len(parts) == 2
+    assert set(parts) == set(
+        [
+            PartNumber(value=467, start=0, end=3),
+            PartNumber(start=2, end=4, value=35),
+        ]
+    )
+
+
+def test_find_pns_two():
+    lines = [
+        # fmt: off
+        ".......................661.........................485..565.......344.......325.....................................841.....725.............",
+        "....*609..131................512.......................*................536*..............462/..-...60..424.........@....$.*................",
+        ".316.........*.......39..................@.630......377........919...........98................789..*..*..............788..2.......=..564...",
+        # fmt: on
+    ]
+    part_numbers = list(find_part_numbers(parse_grid(lines)))
+    numbers = [part.value for part in part_numbers]
+    assert set(numbers) == {316, 609, 131, 377, 565, 536, 325, 462, 789, 60, 424, 841, 788, 2, 725}
+
+
+def test_parse_grid():
+    #   "467..114..",
+    #   "...*......",
+    #   "..35..633.",
+    #   "......#...",
+    #   "617*......",
+    #   ".....+.58.",
+    #   "..592.....",
+    #   "......755.",
+    #   "...$.*....",
+    #   ".664.598..",
+    expected = [
+        [
+            PartNumber(value=467, start=0, end=3),
+            PartNumber(value=114, start=5, end=8),
+        ],
+        [Symbol(start=3, end=4, name="*")],
+        [
+            PartNumber(start=2, end=4, value=35),
+            PartNumber(start=6, end=9, value=633),
+        ],
+        [Symbol(start=6, end=7, name="#")],
+        [PartNumber(start=0, end=3, value=617), Symbol(start=3, end=4, name="*")],
+        [
+            Symbol(start=5, end=6, name="+"),
+            PartNumber(start=7, end=9, value=58),
+        ],
+        [
+            PartNumber(start=2, end=5, value=592),
+        ],
+        [
+            PartNumber(start=6, end=9, value=755),
+        ],
+        [
+            Symbol(start=3, end=4, name="$"),
+            Symbol(start=5, end=6, name="*"),
+        ],
+        [
+            PartNumber(start=1, end=4, value=664),
+            PartNumber(start=5, end=8, value=598),
+        ],
+    ]
+
+    assert parse_grid(toy_input) == expected
+
+
+def test_part_1_toy():
+    assert part_1(toy_input) == 4361
+
+
+def test_part_1_real():
+    assert part_1(input) == "FIXME"
 
 
 # def test_part_2_toy():
