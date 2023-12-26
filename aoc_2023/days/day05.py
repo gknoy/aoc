678a326d9c6c4b1a74506942c5e39ae1146bea54
Gabriel Knoy <gabriel.knoy@gmail.com>
2023-12-25 18:52:44 -0800

    Add Mapping, MapSegment

--- aoc_2023/days/day05.py
+++ aoc_2023/days/day05.py
@@ -1,6 +1,8 @@
 """
 # https://adventofcode.com/2023/day/5
 """
+from dataclasses import dataclass
+from functools import cached_property
 from utils.utils import get_line_items
 
 input = list(get_line_items("aoc_2023/input/05.txt"))
@@ -48,7 +50,90 @@ toy_input: list[str] = [
 # ------------------
 # Every type of seed, soil, fertilizer and so on is identified with a number
 # Numbers are reused by each category
-# soil 123 and fertilizer 123 aren't necessarily related\
+# soil 123 and fertilizer 123 aren't necessarily related
+#
+# NOTE: A plain dict is probably not sufficient, as valaue ranges are _really big_:
+#
+#   seed-to-soil map:
+#   357888202 777841571 45089383
+#   1091769591 2222785614 212172358
+#   747211456 668867483 108974088
+#   # ....
+
+
+@dataclass
+class MapSegment:
+    """
+    (dest range start, source range start, range count)
+
+    intended usage:
+    for segment in segment:
+        if input in segment:
+            return segment[input]
+    return input
+    """
+
+    dest_start: int
+    source_start: int
+    size: int
+
+    @cached_property
+    def dest_end(self):
+        return self.dest_start + self.size
+
+    @cached_property
+    def source_end(self):
+        return self.source_start + self.size
+
+    def __contains__(self, source_index):
+        return self.source_start <= source_index < self.source_end
+
+    def __len__(self):
+        return self.size
+
+    def __lt__(self, other):
+        # The sort algorithm uses only < comparisons between items
+        # We only care about source mapping indexes so that we can
+        return self.source_start < other.source_start
+
+    def __getitem__(self, index):
+        """Used when checking whether input is in range that we handle"""
+        if index < self.source_start or index >= self.source_end:
+            raise IndexError(
+                f"Segment intput range ({self.source_start}, {self.source_start}) "
+                f"does not contain [{index}]"
+            )
+        delta = index - self.source_start
+        assert delta <= self.size, "delta > range_count"
+        return self.dest_start + delta
+
+
+class Mapping:
+    """
+    Take a list of mapping segments
+      (dest range start, source range start, range count)
+    50 98 2
+    52 50 48
+    """
+
+    def __init__(self, mappings):
+        self.map_segments = list(sorted(mappings))
+
+    def __getitem__(self, index):
+        # get the "destination index" when we query an input index
+        for mapping in self.map_segments:
+            print(f">>> segment: {(mapping.source_start, mapping.source_end)}")
+            if index in mapping:
+                return mapping[index]
+        # If we don't have a custom mapping for this index,
+        # then it maps to the same destination
+        print(f"un-mapped index {index}")
+        return index
+
+
+def parse_segment(line):
+    dest_start, source_start, size = [int(item) for item in line.split()]
+    return MapSegment(dest_start=dest_start, source_start=source_start, size=size)
 
 
 def part_1(input, verbose=False):
--- aoc_2023/tests/test_day05.py
+++ aoc_2023/tests/test_day05.py
@@ -1,15 +1,59 @@
 """
 # https://adventofcode.com/2023/day/5
 """
-from aoc_2023.days.day05 import input, toy_input, part_1, part_2
+from aoc_2023.days.day05 import input, toy_input, part_1, part_2, Mapping, MapSegment
 
 
-def test_part_1_toy():
-    assert part_1(toy_input) == 35
+def test_map_segment():
+    a = MapSegment(50, 98, 2)
+    b = MapSegment(52, 50, 48)
+
+    assert b < a  # b's inputs are for an earlier sequence than a's inputs
+
+    assert 98 in a
+    assert 99 in a
+    assert 97 not in a
+    assert 100 not in a
+    assert a[98] == 50
+    assert a[99] == 51
+
+    assert 49 not in b
+    assert 50 in b
+    assert 97 in b
+    assert 98 not in b
+    for k in range(50, 98):  # not including 98
+        assert b[k] == k + 2
 
 
-def test_part_1_real():
-    assert part_1(input) == "FIXME"
+def test_mapping():
+    a = MapSegment(50, 98, 2)
+    b = MapSegment(52, 50, 48)
+
+    m = Mapping([a, b])
+    assert m.map_segments == [b, a]  # sorted but likeley not needed to be
+
+    # un-mapped keys map to same destination
+    assert m[-1] == -1
+    assert m[1000] == 1000
+    for k in range(0, 50):
+        assert m[k] == k
+
+    # b's mapped items:
+    for k in range(50, 98):
+        assert m[k] == k + 2
+        assert m[k] == b[k]
+
+    # a's mapped items work:
+    assert m[98] == 50
+    assert m[99] == 51
+
+
+# def test_part_1_toy():
+#     assert part_1(toy_input) == 35
+
+
+# def test_part_1_real():
+#     assert part_1(input) == "FIXME"
 
 
 # def test_part_2_toy():
