50473f45deaadae775744f9fe2ce0d09add283bb
Gabriel Knoy <gabriel.knoy@gmail.com>
2024-12-24 15:56:46 -0800

    Update test template to fail w/ encountered value

--- aoc_2024/tests/template.py
+++ aoc_2024/tests/template.py
@@ -5,16 +5,20 @@ from aoc_2024.days.day{NN} import input, toy_input, part_1, part_2
 
 
 def test_part_1_toy():
-    assert part_1(toy_input) == "FIXME"
+    value = part_1(toy_input)
+    assert value == "FIXME", value
 
 
 def test_part_1_real():
-    assert part_1(input) == "FIXME"
+    value = part_2(input)
+    assert value == "FIXME", value
 
 
 def test_part_2_toy():
-    assert part_2(toy_input) == "FIXME"
+    value = part_2(toy_input)
+    assert value == "FIXME", value
 
 
 def test_part_2_real():
-    assert part_2(input) == "FIXME"
+    value = part_2(input)
+    assert value == "FIXME", value
